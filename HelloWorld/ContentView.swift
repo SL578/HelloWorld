cc3d6fd95bbf709f078ebf7ba9ced27d210d0a9f
SL578 <linstephen578@gmail.com>
2024-01-27 21:08:34 -0800

    01272024

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -7,17 +7,45 @@
 
 import SwiftUI
 import SwiftData
+import AVFoundation
 
 struct ContentView: View {
-
     var body: some View {
-        Text("Hello world!")
-        Spacer()
-        Text("2")
-        Text("3")
-        Spacer()
-        Spacer()
+        VStack {
+            Button {
+                let utterance = AVSpeechUtterance(string: "Hello World")
+                utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+                let synthesizer = AVSpeechSynthesizer()
+                synthesizer.speak(utterance)
+            } label: {
+                Text("Hello, world.")
+                    .fontWeight(.heavy)
+                    .font(.system(.title, design: .rounded))
+            }
+            .padding()
+            .foregroundColor(.orange)
+            .background(Color.black)
+            .cornerRadius(15)
+            
+            Button {
+                let utterance = AVSpeechUtterance(string: "Hello World")
+                utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+                let synthesizer = AVSpeechSynthesizer()
+                synthesizer.speak(utterance)
+            } label: {
+                Text("Hello, world.2")
+                    .fontWeight(.heavy)
+                    .font(.system(.title, design: .rounded))
+            }
+            .padding()
+            .foregroundColor(.orange)
+            .background(Color.black)
+            .cornerRadius(15)
+        }
     }
+    
+    
+
 }
 #Preview {
     ContentView()
